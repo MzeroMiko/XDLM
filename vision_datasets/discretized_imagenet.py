f4c9d779d9291ba475dd016928cf8886897b5178
MzeroMiko <3496274007@qq.com>
2026-01-30 23:35:33 +0800

    modify indent for vision dataset

--- vision_datasets/discretized_imagenet.py
+++ vision_datasets/discretized_imagenet.py
@@ -45,7 +45,9 @@ class VQGANTok(nn.Module):
         self.vae = vqgan.VQModel(**self.vae_config).cpu()
         if checkpoint is not None:
             assert vocab_size in [1024, 16384]
-            info = self.vae.load_state_dict(torch.load(checkpoint, map_location="cpu")["state_dict"], strict=False)
+            info = self.vae.load_state_dict(
+                torch.load(checkpoint, map_location="cpu")["state_dict"], strict=False
+            )
             print(info, flush=True)
 
     def batch_encode(self, x: torch.Tensor):
@@ -56,7 +58,7 @@ class VQGANTok(nn.Module):
         # token_id = info[-1].reshape(b, xh, xw)
         token_id = info[-1]
         return token_id.view(b, -1)
-    
+
     def batch_decode(self, x: torch.Tensor):
         self.vae.eval()
         b, l = x.shape
@@ -69,19 +71,21 @@ class VQGANTok(nn.Module):
         dec = self.vae.decode(quant)
         b, c, h, w = dec.shape
         return dec
-        
-        
+
+
 class LlamaGenTok(VQGANTok):
     def __init__(self, vocab_size, checkpoint=None):
         nn.Module.__init__(self)
         self.vocab_size = vocab_size
-        self.vae = VQ_models['VQ-16'](codebook_size=vocab_size).cpu()
+        self.vae = VQ_models["VQ-16"](codebook_size=vocab_size).cpu()
         if checkpoint is not None:
             assert vocab_size in [16384]
-            info = self.vae.load_state_dict(torch.load(checkpoint, map_location="cpu")["model"], strict=False)
+            info = self.vae.load_state_dict(
+                torch.load(checkpoint, map_location="cpu")["model"], strict=False
+            )
             print(info, flush=True)
-   
-    
+
+
 def build_image_tokenizer(vocab_size, checkpoint):
     if "vq_ds16_c2i.pt" in checkpoint:
         return LlamaGenTok(vocab_size, checkpoint=checkpoint)
@@ -109,10 +113,10 @@ class ImageNetVisionTokenizer:
             self.vocab_size = self.vocab_size + 2  # mask token, bos_token, eos_token
         else:
             self.vocab_size = self.vocab_size
-        
+
         self.dummy = True
         self.tokenizer = build_image_tokenizer(vocab_size, checkpoint=checkpoint)
-        
+
     def __len__(self):
         return self.vocab_size
 
@@ -130,7 +134,13 @@ def load_wnids(file_list):
 def load_imagenet_val(file_list, root, wnid_to_idx):
     with open(file_list, "r") as f:
         vlist = f.readlines()[1:]
-    samples = [(os.path.join(root, v.split(",")[0] + ".JPEG"), wnid_to_idx[v.split(",")[1].split(" ")[0]]) for v in vlist]
+    samples = [
+        (
+            os.path.join(root, v.split(",")[0] + ".JPEG"),
+            wnid_to_idx[v.split(",")[1].split(" ")[0]],
+        )
+        for v in vlist
+    ]
     targets = [s[1] for s in samples]
     return samples, targets
 
@@ -140,8 +150,12 @@ class ImageNet(torchvision.datasets.ImageNet):
         if train:
             super().__init__(root=root, split="train")
         else:
-            file_list = os.path.join(os.path.dirname(__file__), "imagenet/LOC_val_solution.csv")
-            self.wnids = load_wnids(os.path.join(os.path.dirname(__file__), "imagenet/class_wnids.txt"))
+            file_list = os.path.join(
+                os.path.dirname(__file__), "imagenet/LOC_val_solution.csv"
+            )
+            self.wnids = load_wnids(
+                os.path.join(os.path.dirname(__file__), "imagenet/class_wnids.txt")
+            )
             root = self.root = os.path.expanduser(root)
             self.split = "val"
             self.parse_archives()
@@ -149,23 +163,27 @@ class ImageNet(torchvision.datasets.ImageNet):
             self.root = root
             self.wnid_to_idx = {x: i for i, x in enumerate(self.wnids)}
             self.classes = [wnid_to_classes[wnid] for wnid in self.wnids]
-            self.class_to_idx = {cls: idx for idx, clss in enumerate(self.classes) for cls in clss}
-            self.samples, self.targets = load_imagenet_val(file_list, root=os.path.join(root, "val"), wnid_to_idx=self.wnid_to_idx)
+            self.class_to_idx = {
+                cls: idx for idx, clss in enumerate(self.classes) for cls in clss
+            }
+            self.samples, self.targets = load_imagenet_val(
+                file_list, root=os.path.join(root, "val"), wnid_to_idx=self.wnid_to_idx
+            )
             self.loader = torchvision.datasets.folder.default_loader
             self.target_transform = None
             self.transform = None
         assert len(self.wnid_to_idx) == 1000, f"{len(self.wnid_to_idx)}"
-            
-        self.transform = torchvision.transforms.Compose([
-            torchvision.transforms.Resize(256),
-            torchvision.transforms.CenterCrop(256),
-            torchvision.transforms.RandomHorizontalFlip(0.5 if hflip else -1.0),
-            torchvision.transforms.ToTensor(),
-        ])
+
+        self.transform = torchvision.transforms.Compose(
+            [
+                torchvision.transforms.Resize(256),
+                torchvision.transforms.CenterCrop(256),
+                torchvision.transforms.RandomHorizontalFlip(0.5 if hflip else -1.0),
+                torchvision.transforms.ToTensor(),
+            ]
+        )
 
     def __getitem__(self, index):
         sample, target = super().__getitem__(index)
-        sample = sample * 2 - 1 # should be [-1, 1] based on taming-transformer
+        sample = sample * 2 - 1  # should be [-1, 1] based on taming-transformer
         return {"input_ids": sample, "labels": target, "tovae": True}
-
-
