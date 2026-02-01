7376cef02015779b809ed114e29438514de097d6
MzeroMiko <3496274007@qq.com>
2026-02-01 14:16:06 +0800

    add dataloader/sampler

--- dataloader.py
+++ dataloader.py
@@ -22,7 +22,6 @@ import utils
 from vision_datasets.discretized_cifar10 import DummyVisionTokenizer, DiscreteCIFAR10
 from vision_datasets.discretized_imagenet import ImageNetVisionTokenizer, ImageNet
 
-
 LOGGER = utils.get_logger(__name__)
 
 
@@ -735,13 +734,9 @@ def get_dataloaders(
         train_set = None
     else:
         if "cifar10" in config.data.train:
-            train_set = DiscreteCIFAR10(
-                config.data.train, train=True, download=True
-            )
+            train_set = DiscreteCIFAR10(config.data.train, train=True, download=True)
         elif "imagenet" in config.data.tokenizer_name_or_path:
-            train_set = ImageNet(
-                config.data.train, train=True
-            )
+            train_set = ImageNet(config.data.train, train=True)
         else:
             train_set = get_dataset(
                 config.data.train,
@@ -764,13 +759,9 @@ def get_dataloaders(
         valid_set = None
     else:
         if "cifar10" in config.data.valid:
-            valid_set = DiscreteCIFAR10(
-                config.data.valid, train=False, download=True
-            )
+            valid_set = DiscreteCIFAR10(config.data.valid, train=False, download=True)
         elif "imagenet" in config.data.tokenizer_name_or_path:
-            valid_set = ImageNet(
-                config.data.valid, train=False
-            )
+            valid_set = ImageNet(config.data.valid, train=False)
         else:
             valid_set = get_dataset(
                 config.data.valid,
@@ -820,3 +811,108 @@ def get_dataloaders(
 
     return train_loader, valid_loader
 
+
+# Samplers adapted from: https://github.com/Dao-AILab/flash-attention/blob/main/training/src/datamodules/fault_tolerant_sampler.py
+
+
+class RandomFaultTolerantSampler(torch.utils.data.RandomSampler):
+
+    def __init__(self, *args, generator=None, **kwargs):
+        # TD [2022-07-17]: We don't force the seed to be zero. We generate random seed,
+        # which should be reproducible if pl.seed_everything was called beforehand.
+        # This means that changing the seed of the experiment will also change the
+        # sampling order.
+        if generator is None:
+            seed = int(torch.empty((), dtype=torch.int64).random_().item())
+            generator = torch.Generator().manual_seed(seed)
+        kwargs.pop("shuffle", None)
+        super().__init__(*args, generator=generator, **kwargs)
+        self.counter = 0
+        self.restarting = False
+
+    def state_dict(self):
+        return {"random_state": self.generator.get_state(), "counter": self.counter}
+
+    def load_state_dict(self, state_dict):
+        self.generator.set_state(state_dict.get("random_state"))
+        self.counter = state_dict["counter"]
+        # self.start_counter = self.counter
+        self.restarting = True
+
+    # TD [2022-08-28] Setting the len will cause PL to think there are only a few batches left per
+    # epoch, and subsequent epoch will have very few batches.
+
+    def __iter__(self) -> typing.Iterator[int]:
+        n = len(self.data_source)
+
+        self.state = self.generator.get_state()
+        indices = torch.randperm(n, generator=self.generator).tolist()
+
+        if not self.restarting:
+            self.counter = 0
+        else:
+            indices = indices[self.counter :]
+            self.restarting = False
+
+        for index in indices:
+            self.counter += 1
+            yield index
+
+        self.counter = 0
+
+
+class FaultTolerantDistributedSampler(torch.utils.data.DistributedSampler):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.counter = 0
+        self.restarting = False
+
+    def state_dict(self):
+        return {"epoch": self.epoch, "counter": self.counter}
+
+    def load_state_dict(self, state_dict):
+        self.epoch = state_dict["epoch"]
+        self.counter = state_dict["counter"]
+        self.restarting = True
+
+    # TD [2022-08-28] Setting the len will cause PL to think there are only a few batches left per
+    # epoch, and subsequent epoch will have very few batches.
+    def __iter__(self):
+        if self.shuffle:
+            # deterministically shuffle based on epoch and seed
+            g = torch.Generator()
+            g.manual_seed(self.seed + self.epoch)
+            indices = torch.randperm(len(self.dataset), generator=g).tolist()  # type: ignore[arg-type]
+        else:
+            indices = list(range(len(self.dataset)))  # type: ignore[arg-type]
+
+        if not self.drop_last:
+            # add extra samples to make it evenly divisible
+            padding_size = self.total_size - len(indices)
+            if padding_size <= len(indices):
+                indices += indices[:padding_size]
+            else:
+                indices += (indices * math.ceil(padding_size / len(indices)))[
+                    :padding_size
+                ]
+        else:
+            # remove tail of data to make it evenly divisible.
+            indices = indices[: self.total_size]
+        assert len(indices) == self.total_size
+
+        # subsample
+        indices = indices[self.rank : self.total_size : self.num_replicas]
+        assert len(indices) == self.num_samples
+
+        if not self.restarting:
+            self.counter = 0
+        else:
+            indices = indices[self.counter :]
+            self.restarting = False
+
+        for index in indices:
+            self.counter += 1
+            yield index
+
+        self.counter = 0
